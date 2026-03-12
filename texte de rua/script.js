06409d5713a87dcc937132983f5939b1e1a4967b
progamadorvinicius <vinicius.brow244@gmail.com>
2026-03-12 10:48:04 +0000

    subway

--- texte de rua/script.js
+++ texte de rua/script.js
@@ -2,3 +2,11 @@ let confirmButton = document.querySelector(".confirm-btn")
 confirmButton.addEventListener("click", () => {
     alert("Delivery Confirmado!")
 })
+
+const items = document.querySelectorAll(".item")
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("selected")
+    })
+
+})
